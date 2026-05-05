f8ea78e41b36ea147e4f2b977e5198bab204b3fa
natiqhaciyef <natiq00h2272@gmail.com>
2026-05-05 22:53:22 +0400

    Initial basics of python learning in here

---
+++ main.py
@@ -0,0 +1,17 @@
+# age = 23
+# name = "Natig"
+# isOnline = True
+# price = 33.33
+
+# print("Age:", age)
+# print("Name:", name)
+
+# birthYear = input("Enter your birth year: ")
+# age = 2026 - int(birthYear)
+# print("Age is:", age)
+
+first = input("First: ")
+second = input("Second: ")
+sum = float(first) + float (second)
+print("Sum: ", sum)
+print("Sum: " + str(sum))
