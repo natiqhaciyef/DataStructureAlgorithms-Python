6426166886a3ec9f840563bed288a4485d35fb5d
natighajiyev <natig.s.hajiyev@abb-bank.az>
2026-05-06 15:46:20 +0400

    Basics of python implemented

---
+++ basics/Numbers.py
@@ -0,0 +1,3 @@
+a = 12 # integer number
+b = 10.5 # floating number
+c = 1 + 2j # complex number
--- basics/String Functions.py
+++ basics/String Functions.py
@@ -1,4 +1,12 @@
 # STRING FUNCTIONS
 course = 'Python is perfect'
-course.upper()
-print('Python' in course)
+course.upper() # makes all letters uppercase
+course.lower() # makes all letters lowercase
+course.title() # changes first letters uppercase for each word in string
+course.strip() # removes white spaces from beginning and ending part of string
+course.lstrip() # removes white spaces from beginning part of string
+course.rstrip() # removes white spaces from ending part of string
+course.find("per") # finds the starting index of given substring. if not found it return -1
+course.replace("thon", "charm") # changes "thon" substring with "charm"
+print("charm" in course)
+print("thon" not in course)
